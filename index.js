776e84754030f900a70c221907c095424af31d9f
d-pozniak <pozniakdi@gmail.com>
2022-09-23 01:11:18 +0300

    prettified code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { print } from './js/lib.js';
+import { print } from "./js/lib.js";
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
@@ -8,43 +8,43 @@ import { print } from './js/lib.js';
 
 // ======== OBJECTS DEFINITIONS ========
 const dog = {
-    species: 'dog',
+    species: "dog",
     hands: 0,
-    legs : 4,
-    name: 'Bill',
-    gender:'male',
-    says: 'Woof!',
+    legs: 4,
+    name: "Bill",
+    gender: "male",
+    says: "Woof!",
 };
 const woman = {
-    species: 'human',
-    hands: '2',
-    legs : '2',
-    name: 'Pam',
-    gender:'female',
-    says: 'Kottans` community is the best!',
+    species: "human",
+    hands: "2",
+    legs: "2",
+    name: "Pam",
+    gender: "female",
+    says: "Kottans` community is the best!",
 };
 const cat = {
-    species: 'cat',
+    species: "cat",
     hands: 0,
-    legs : 4,
-    name: 'Mattew',
-    gender:'male',
-    says: 'Meow!',
+    legs: 4,
+    name: "Mattew",
+    gender: "male",
+    says: "Meow!",
 };
 const man = {
-    species: 'human',
-    hands: '2',
-    legs : '2',
-    name: 'Paul',
-    gender:'male',
-    says: 'Why don`t we go grab something to eat?',
+    species: "human",
+    hands: "2",
+    legs: "2",
+    name: "Paul",
+    gender: "male",
+    says: "Why don`t we go grab something to eat?",
 };
 const catWoman = Object.create(cat, {
-    species: { value:'hero'},
-    hands: { value:'2'},
-    legs : {value:'2'},
-    name: {value : 'Sally'},
-    gender:{value : 'female'},
+    species: { value: "hero" },
+    hands: { value: "2" },
+    legs: { value: "2" },
+    name: { value: "Sally" },
+    gender: { value: "female" },
 });
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -54,13 +54,12 @@ const catWoman = Object.create(cat, {
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.
    */
-const keyList = ['species', 'name', 'gender', 'legs', 'hands', 'says'];
+const keyList = ["species", "name", "gender", "legs", "hands", "says"];
 const habList = [man, woman, cat, dog, catWoman];
 function createString(obj, keys) {
-    return keys.map(key => obj[key])
-               .join('; ');
+    return keys.map((key) => obj[key]).join("; ");
 }
-function printWorldHabitants(objects, features){
-    objects.forEach(obj => print(createString(obj, features), 'div'))
+function printWorldHabitants(objects, features) {
+    objects.forEach((obj) => print(createString(obj, features), "div"));
 }
 printWorldHabitants(habList, keyList);
