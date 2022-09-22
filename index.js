d64144ce7e8d2f02687857c0bdc0efae32150beb
d-pozniak <pozniakdi@gmail.com>
2022-09-23 00:58:49 +0300

    basic requirements satisfyed

--- index.js
+++ index.js
@@ -2,14 +2,50 @@ import { print } from './js/lib.js';
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/d-pozniak/a-tiny-JS-world
+   Web app: https://d-pozniak.github.io/a-tiny-JS-world/
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-
+const dog = {
+    species: 'dog',
+    hands: 0,
+    legs : 4,
+    name: 'Bill',
+    gender:'male',
+    says: 'Woof!',
+};
+const woman = {
+    species: 'human',
+    hands: '2',
+    legs : '2',
+    name: 'Pam',
+    gender:'female',
+    says: 'Kottans` community is the best!',
+};
+const cat = {
+    species: 'cat',
+    hands: 0,
+    legs : 4,
+    name: 'Mattew',
+    gender:'male',
+    says: 'Meow!',
+};
+const man = {
+    species: 'human',
+    hands: '2',
+    legs : '2',
+    name: 'Paul',
+    gender:'male',
+    says: 'Why don`t we go grab something to eat?',
+};
+const catWoman = Object.create(cat, {
+    species: { value:'hero'},
+    hands: { value:'2'},
+    legs : {value:'2'},
+    name: {value : 'Sally'},
+    gender:{value : 'female'},
+});
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -18,13 +54,13 @@ import { print } from './js/lib.js';
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.
    */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+const keyList = ['species', 'name', 'gender', 'legs', 'hands', 'says'];
+const habList = [man, woman, cat, dog, catWoman];
+function createString(obj, keys) {
+    return keys.map(key => obj[key])
+               .join('; ');
+}
+function printWorldHabitants(objects, features){
+    objects.forEach(obj => print(createString(obj, features), 'div'))
+}
+printWorldHabitants(habList, keyList);
